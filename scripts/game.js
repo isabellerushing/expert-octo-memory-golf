a64d46fb5ac16c077b4571194a7691c1940bb388
Mandy McGinnis <157517770+mmandelyn@users.noreply.github.com>
2024-03-22 18:55:04 -0500

    Add files via upload

---
+++ scripts/game.js
@@ -0,0 +1,79 @@
+const alphaGames = [
+  {name:"Colby Norwood", 
+   gameName:"Alpha Lab",
+   url:"https://cnorwood10.github.io/stunning-adventure-alfa-cis376/"},
+
+  {name:"Levon Watkins", 
+   gameName:"Alpha Lab",
+   url:"https://v-sec0.github.io/azura-alfa-lab/"},
+
+  {name:"Dalton Rodgers", 
+   gameName:"Alpha Lab",
+   url:"https://daltrodg.github.io/miniature-octo-palm-alpha/"}
+];
+
+const charlieGames = [
+  {name:"Lucas Hasting", 
+   gameName:"Doge Game",
+   url:"https://lucashasting.github.io/Doge-Game/"},
+
+  {name:"Ella Rohling", 
+   gameName:"Charlie Lab",
+   url:"https://ellarohling.github.io/bookish-umbrella-charlie-fall2023/"},
+
+  {name:"Raiden Greene", 
+   gameName:"Charlie Lab",
+   url:"https://raidengreene.github.io/charlielabfall2023-ideal-garbanzo"}
+];
+
+const deltaGames = [
+  {name:"Janna Parker", 
+   gameName:"Delta Lab",
+   url:"https://jparker13.github.io/silver-computing-machine-DELTA/"},
+
+  {name:"Jacob Rast", 
+   gameName:"Delta Lab",
+   url:"https://rastifer.github.io/animated-octo-dollop-delta-jrast/"},
+   
+  {name:"Johnny Caringi", 
+   gameName:"Delta Lab",
+   url:"https://johnnycaringi.github.io/Spirit-Animal-Finder/"},
+  
+  {name:"Elizabeth Tucker", 
+   gameName:"Delta Lab",
+   url:"https://beth-ann-tucker.github.io/silver-octo-meme-delta-lab/"},
+  
+  {name:"Rachel Thompson", 
+   gameName:"Delta Lab",
+   url:"https://github.com/panhead63/friendly-octo-guide-delta"},
+
+  {name:"Logan Beckman", 
+   gameName:"Delta Lab",
+   url:"https://loganbeckman21.github.io/urban-invention-delta/"}
+];
+
+const echoGames =[
+  {name:"Slade Berryman", 
+   gameName:"Echo Lab",
+   url:"https://sladeberryman.github.io/echo-376/"},
+  
+  {name:"Rachel Honeycutt", 
+   gameName:"Echo Lab",
+   url:"https://rachelhoneycutt.github.io/ideal-couscous-ECHO/"},
+  
+  {name:"Seungyeon Chang", 
+   gameName:"Echo Lab",
+   url:"https://seungyeonchang.github.io/echo_lab_2023/"},
+
+  {name:"Brandon Jacks", 
+   gameName:"Echo Lab",
+   url:"https://bjacks01.github.io/brandoEcho/"},
+  
+  {name:"Kaitlin Renfroe", 
+   gameName:"Echo Lab",
+   url:"https://kaitlinrenfroe.github.io/renfroe-echo-lab/"},
+
+  {name:"Ryan McDonald", 
+   gameName:"Echo Lab",
+   url:"https://azyn7.github.io/Echo-Lab/"}
+];
