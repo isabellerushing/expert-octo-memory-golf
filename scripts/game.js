058f25cf223cd3d027e80e78fbfe4f2225c5445d
Mandy McGinnis <157517770+mmandelyn@users.noreply.github.com>
2024-03-22 21:17:06 -0500

    Add files via upload

--- scripts/game.js
+++ scripts/game.js
@@ -1,206 +1,42 @@
-const charlieGames = [
-  {name:"Devan Bush", 
-   gameName:" improved-octo-chainsaw-charlie",
-   url:"https://github.com/devan4264/improved-octo-chainsaw-charlie"},
-
-  {name:"Mackenzie Thompson", 
-   gameName:"legendary-goggles-charlie",
-   url:"https://github.com/mackenzie24t/legendary-goggles-charlie"},
-
-  {name:"John Blaxton", 
-   gameName:"crispy-charlie",
-   url:"https://github.com/JohnAllenB/crispy-charlie-lab03-blaxton"},
-
-  {name:"Kalani Sorensen", 
-   gameName:"charlie-lab-cis376",
-   url:"https://github.com/babayaga1013/charlie-lab-cis376"},
-
-  {name:"Jacob Vance", 
-   gameName:"turbo-pancake-charlie",
-   url:"https://jacob-vance.github.io/turbo-pancake-charlie/"},
-
-  {name:"Jayden Green", 
-   gameName:"fictional-broccoli-charlie",
-   url:"https://jaydengreen23.github.io/fictional-broccoli-charlie/"},
-
-  {name:"Sam Maldonado", 
-   gameName:"Hot or Cold",
-   url:"https://smaldonado1326.github.io/cuddly-invention--charlie/"},
-
-  {name:"Nolen Robinson", 
-   gameName:"Charlie Meme",
-   url:"https://nrobinson4.github.io/bug-free-potato-charlie/"},
-
-  {name:"Michael Duran", 
-   gameName:"Charlie Lab Doge Game",
-   url:"https://github.com/MichaelHaydenDuran/silly-dino-charlie"},
-
-  {name:"Alex Glenn", 
-   gameName:"charlie-project-alex",
-   url:"https://github.com/pglenn1/charlie-project-alex"},
-
-  {name:"Kevin Hernandez", 
-   gameName:"Charlie-BravoSoMuchWow",
-   url:"https://hkevin555.github.io/lab-charlie/"},
-
-  {name:"Rodnesia Goodloe", 
-   gameName:"nesia-charlie-lab",
-   url:"https://github.com/nesiagoodloe/nesia-charlie-lab"},
-
-  {name:"Claire Michell", 
-   gameName:"Charlie",
-   url:"https://cm1803.github.io/charlie/"},
-
-  {name:"Saleh Basalim", 
-   gameName:"Charlie-lab",
-   url:"https://salehmb89.github.io/waffle-charlie/"},
-
-  {name:"Mandy MiGinnis", 
-   gameName:"Hopping Zone Charlie",
-   url:"https://github.com/mmandelyn/silver-seal-charlie"},
-
-  {name:"Skyler Saunders", 
-   gameName:"super-adventure-charlie",
-   url:"https://skysaunders.github.io/super-adventure-charlie/"},
-
-  {name:"Mohammed Alrasheidi", 
-   gameName:"Charlie",
-   url:"https://mohammedalrasheidi.github.io/Delta-lab/"},
-
-  {name:"Ayman Alluhaybi", 
-   gameName:"CHARLIE",
-   url:"https://aalluhaybi1.github.io/CHARLIE_LAB/"},
-
-  {name:"Brayden Bolton", 
-   gameName:"Charlie",
-   url:"https://github.com/bbolton1/CIS376-Charlie-Brayden"},
-
-  {name:"Navraj Singh", 
-   gameName:"probable-charlie",
-   url:"https://github.com/QuantumCoder-242/probable-charlie"},
-
-  {name:"Akashdeep Singh", 
-   gameName:"Chrlie-lab-By-Akashdeep",
-   url:"https://github.com/AkashdeepSingh0047/Chrlie-lab-By-Akashdeep?tab=readme-ov-file"},
-
-  {name:"Zach Rundle", 
-   gameName:"charlie-lab-zrundle",
-   url:"https://github.com/zrundle/charlie-lab-zrundle"},
-
-  {name:"Isabelle Rushing", 
-   gameName:"Charlie",
-   url:"https://isabellerushing.github.io/improved-octo-giggle-charlie/"},
-];
-
-const deltaGames = [
-  {name:"Devan Bush", 
-   gameName:"verbose-guacamole-delta",
-   url:"https://github.com/devan4264/verbose-guacamole-delta"},
-
-  {name:"Mackenzie Thompson", 
-   gameName:"delta-jubilant-doodle",
-   url:"https://github.com/mackenzie24t/delta-jubilant-doodle"},
-
-  {name:"John Blaxton", 
-   gameName:"decatur-delta",
-   url:"https://github.com/JohnAllenB/decatur-delta-lab04-blaxton"},
-
-  {name:"Kalani Sorensen", 
-   gameName:"reimagined-couscous–delta",
-   url:"https://github.com/babayaga1013/reimagined-couscous--delta"},
-
-  {name:"Jacob Vance", 
-   gameName:"fluffy-pancake-delta",
-   url:"https://jacob-vance.github.io/fluffy-pancake-delta/"},
-
-  {name:"Jayden Green", 
-   gameName:"delta-jubilant-octo-winner",
-   url:"https://jaydengreen23.github.io/delta-jubilant-octo-winner/"},
-
-  {name:"Sam Maldonado", 
-   gameName:"Name Game",
-   url:"https://smaldonado1326.github.io/glowing-adventure-delta/"},
-
-  {name:"Nolen Robinson", 
-   gameName:"Delta Names",
-   url:"https://nrobinson4.github.io/cool-animal-delta/"},
-
-  {name:"Michael Duran", 
-   gameName:"The Mystical Lucky Eccentric Silly Llama Game!",
-   url:"https://github.com/MichaelHaydenDuran/chica-bonita-delta"},
-
-  {name:"Alex Glenn", 
-   gameName:"delta-project-alex",
-   url:"https://pglenn1.github.io/delta-project-alex/"},
-
-  {name:"Kevin Hernandez", 
-   gameName:"Spirit Animal Name Game",
-   url:"https://hkevin555.github.io/DeltaLab/"},
-
-  {name:"Rodnesia Goodloe", 
-   gameName:"currentDeltaLab",
-   url:"https://github.com/nesiagoodloe/currentDeltaLab"},
-
-  {name:"Clarie Mitchell", 
-   gameName:"Delta",
-   url:"https://cm1803.github.io/delta/"},
-
-  {name:"Marvin Cruz", 
-   gameName:"Delta Lab Spirit animal",
-   url:"https://marvin-2056.github.io/Delta-CIS-367/"},
-
-  {name:"Saleh Basalim", 
-   gameName:"Delta-lab",
-   url:"https://salehmb89.github.io/delta-lab/"},
-
-  {name:"Mandy McGinnis", 
-   gameName:"Spirit Animal Delta",
-   url:"https://github.com/mmandelyn/pink-panda-delta"},
+$(function () {
   
-  {name:"Mohammed Alrasheidi", 
-   gameName:"Delta",
-   url:"https://mohammedalrasheidi.github.io/Delta-lab/"},
 
-  {name:"Grant Hartley", 
-   gameName:"delta",
-   url:"https://github.com/hrantgartley/didactic-waffle-delta"},
-
-  {name:"Ayman Alluhaybi", 
-   gameName:"delta",
-   url:"https://aalluhaybi1.github.io/DELTA-LAB"},
-
-  {name:"Brayden Bolton", 
-   gameName:"Delta",
-   url:"https://github.com/bbolton1/delta-lab-brayden"},
-
-  {name:"Navraj Singh", 
-   gameName:"turbo-delta",
-   url:"https://github.com/QuantumCoder-242/turbo-delta"},
-
-  {name:"Akashdeep Sigh", 
-   gameName:"psychic-delta ",
-   url:"https://github.com/AkashdeepSingh0047/psychic-delta?tab=readme-ov-file"},
-
-  {name:"Zach Rundle", 
-   gameName:"zrundle-delta-lab",
-   url:"https://github.com/zrundle/zrundle-delta-lab"},
-
-  {name:"Isabelle Rushing", 
-   gameName:"delta",
-   url:"https://isabellerushing.github.io/crispy-octo-carnival-delta/"}
-];
-
-const otherGames = [
-  {name:"Candice Buford", 
-  gameName:"Glowing Giggle Echo Lab",
-  url:"https://cbuf0rd.github.io/glowing-giggle-echo-lab/"},
+    $.each(charlieGames, function (index, data) {
   
-  {name:"Grant Hartley", 
-   gameName:"echo",
-   url:"https://github.com/hrantgartley/jubilant-bassoon-echo"},
-
-  {name:"Skyler Saunders", 
-  gameName:"animated-sniffle-foxtrot",
-  url:"https://skysaunders.github.io/animated-sniffle-foxtrot/"}
-
-];
+      $("#gameRowCharlie").append(
+        '<div class="col-md-4"> <div class="card m-3 text-black" style="background-color:#9ebaf5">' + '<img class="card-img-top" src="https://i.pinimg.com/474x/94/4f/ee/944fee753e59ac050438bfdf65989132.jpg" alt="Gamer Icon">' +
+          '<div class="card-body"> <div class="offset-md-2 col-md-8 mx-auto text-center">' +
+          '<h4 class="card-title">' +data.gameName+ '</h4>' +
+          '<p class="card-text">Creator: '+  data.name +'</p>' +
+          '<a href="' +
+          data.url +
+          '" target="_blank" class="btn text-white" style="background-color:#152238">Click here to play!</a></div> '
+      );
+    });
+    $.each(deltaGames, function (index, data) {
+  
+      $("#gameRowDelta").append(
+        '<div class="col-md-4"> <div class="card m-3 text-black" style="background-color:#9ebaf5">' + '<img class="card-img-top" src="https://i.pinimg.com/474x/94/4f/ee/944fee753e59ac050438bfdf65989132.jpg" alt="Gamer Icon">' +
+          '<div class="card-body"> <div class="offset-md-2 col-md-8 mx-auto text-center">' +
+          '<h4 class="card-title">' +data.gameName+ '</h4>' +
+          '<p class="card-text">Creator: '+  data.name +'</p>' +
+          '<a href="' +
+          data.url +
+          '" target="_blank" class="btn text-white" style="background-color:#152238">Click here to play!</a></div> '
+      );
+    });
+  
+    $.each(otherGames, function (index, data) {
+  
+      $("#gameRowOther").append(
+        '<div class="col-md-4"> <div class="card m-3 text-black" style="background-color:#9ebaf5">' + '<img class="card-img-top" src="https://i.pinimg.com/474x/94/4f/ee/944fee753e59ac050438bfdf65989132.jpg" alt="Gamer Icon">' +
+          '<div class="card-body"> <div class="offset-md-2 col-md-8 mx-auto text-center">' +
+          '<h4 class="card-title">' +data.gameName+ '</h4>' +
+          '<p class="card-text">Creator: '+  data.name +'</p>' +
+          '<a href="' +
+          data.url +
+          '" target="_blank" class="btn text-white" style="background-color:#152238">Click here to play!</a></div> '
+      );
+    });
+    
+  });
