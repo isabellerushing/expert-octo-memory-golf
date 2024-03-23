c9cdba2bba5f9777ebb49f2a577cd07f90141988
nesiagoodloe <157249824+nesiagoodloe@users.noreply.github.com>
2024-03-22 20:18:59 -0500

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,79 +1,207 @@
-const alphaGames = [
-  {name:"Colby Norwood", 
-   gameName:"Alpha Lab",
-   url:"https://cnorwood10.github.io/stunning-adventure-alfa-cis376/"},
-
-  {name:"Levon Watkins", 
-   gameName:"Alpha Lab",
-   url:"https://v-sec0.github.io/azura-alfa-lab/"},
-
-  {name:"Dalton Rodgers", 
-   gameName:"Alpha Lab",
-   url:"https://daltrodg.github.io/miniature-octo-palm-alpha/"}
-];
-
 const charlieGames = [
-  {name:"Lucas Hasting", 
-   gameName:"Doge Game",
-   url:"https://lucashasting.github.io/Doge-Game/"},
+  {name:"Devan Bush", 
+   gameName:" improved-octo-chainsaw-charlie",
+   url:"https://github.com/devan4264/improved-octo-chainsaw-charlie"},
 
-  {name:"Ella Rohling", 
-   gameName:"Charlie Lab",
-   url:"https://ellarohling.github.io/bookish-umbrella-charlie-fall2023/"},
+  {name:"Mackenzie Thompson", 
+   gameName:"legendary-goggles-charlie",
+   url:"https://github.com/mackenzie24t/legendary-goggles-charlie"},
 
-  {name:"Raiden Greene", 
-   gameName:"Charlie Lab",
-   url:"https://raidengreene.github.io/charlielabfall2023-ideal-garbanzo"}
+  {name:"John Blaxton", 
+   gameName:"crispy-charlie",
+   url:"https://github.com/JohnAllenB/crispy-charlie-lab03-blaxton"},
+
+  {name:"Kalani Sorensen", 
+   gameName:"charlie-lab-cis376",
+   url:"https://github.com/babayaga1013/charlie-lab-cis376"},
+
+  {name:"Jacob Vance", 
+   gameName:"turbo-pancake-charlie",
+   url:"https://jacob-vance.github.io/turbo-pancake-charlie/"},
+
+  {name:"Jayden Green", 
+   gameName:"fictional-broccoli-charlie",
+   url:"https://jaydengreen23.github.io/fictional-broccoli-charlie/"},
+
+  {name:"Sam Maldonado", 
+   gameName:"Hot or Cold",
+   url:"https://smaldonado1326.github.io/cuddly-invention--charlie/"},
+
+  {name:"Nolen Robinson", 
+   gameName:"Charlie Meme",
+   url:"https://nrobinson4.github.io/bug-free-potato-charlie/"},
+
+  {name:"Michael Duran", 
+   gameName:"Charlie Lab Doge Game",
+   url:"https://github.com/MichaelHaydenDuran/silly-dino-charlie"},
+
+  {name:"Alex Glenn", 
+   gameName:"charlie-project-alex",
+   url:"https://github.com/pglenn1/charlie-project-alex"},
+
+  {name:"Kevin Hernandez", 
+   gameName:"Charlie-BravoSoMuchWow",
+   url:"https://hkevin555.github.io/lab-charlie/"},
+
+  {name:"Rodnesia Goodloe", 
+   gameName:"nesia-charlie-lab",
+   url:"https://github.com/nesiagoodloe/nesia-charlie-lab"},
+
+  {name:"Claire Michell", 
+   gameName:"Charlie",
+   url:"https://cm1803.github.io/charlie/"},
+
+  {name:"Saleh Basalim", 
+   gameName:"Charlie-lab",
+   url:"https://salehmb89.github.io/waffle-charlie/"},
+
+  {name:"Mandy MiGinnis", 
+   gameName:"Hopping Zone Charlie",
+   url:"https://github.com/mmandelyn/silver-seal-charlie"},
+
+  {name:"Skyler Saunders", 
+   gameName:"super-adventure-charlie",
+   url:"https://skysaunders.github.io/super-adventure-charlie/"},
+
+  {name:"Candice Buford", 
+   gameName:"Glowing Giggle Echo Lab",
+   url:"https://cbuf0rd.github.io/glowing-giggle-echo-lab/"},
+
+  {name:"Mohammed Alrasheidi", 
+   gameName:"Charlie",
+   url:"https://mohammedalrasheidi.github.io/Delta-lab/"},
+
+  {name:"Grant Hartley", 
+   gameName:"echo",
+   url:"https://github.com/hrantgartley/jubilant-bassoon-echo"},
+
+  {name:"Ayman Alluhaybi", 
+   gameName:"CHARLIE",
+   url:"https://aalluhaybi1.github.io/CHARLIE_LAB/"},
+
+  {name:"Brayden Bolton", 
+   gameName:"Charlie",
+   url:"https://github.com/bbolton1/CIS376-Charlie-Brayden"},
+
+  {name:"Navraj Singh", 
+   gameName:"probable-charlie",
+   url:"https://github.com/QuantumCoder-242/probable-charlie"},
+
+  {name:"Akashdeep Singh", 
+   gameName:"Chrlie-lab-By-Akashdeep",
+   url:"https://github.com/AkashdeepSingh0047/Chrlie-lab-By-Akashdeep?tab=readme-ov-file"},
+
+  {name:"Zach Rundle", 
+   gameName:"charlie-lab-zrundle",
+   url:"https://github.com/zrundle/charlie-lab-zrundle"},
+
+  {name:"Isabelle Rushing", 
+   gameName:"Charlie",
+   url:"https://isabellerushing.github.io/improved-octo-giggle-charlie/"},
 ];
 
 const deltaGames = [
-  {name:"Janna Parker", 
-   gameName:"Delta Lab",
-   url:"https://jparker13.github.io/silver-computing-machine-DELTA/"},
+  {name:"Devan Bush", 
+   gameName:"verbose-guacamole-delta",
+   url:"https://github.com/devan4264/verbose-guacamole-delta"},
 
-  {name:"Jacob Rast", 
-   gameName:"Delta Lab",
-   url:"https://rastifer.github.io/animated-octo-dollop-delta-jrast/"},
-   
-  {name:"Johnny Caringi", 
-   gameName:"Delta Lab",
-   url:"https://johnnycaringi.github.io/Spirit-Animal-Finder/"},
-  
-  {name:"Elizabeth Tucker", 
-   gameName:"Delta Lab",
-   url:"https://beth-ann-tucker.github.io/silver-octo-meme-delta-lab/"},
-  
-  {name:"Rachel Thompson", 
-   gameName:"Delta Lab",
-   url:"https://github.com/panhead63/friendly-octo-guide-delta"},
+  {name:"Mackenzie Thompson", 
+   gameName:"delta-jubilant-doodle",
+   url:"https://github.com/mackenzie24t/delta-jubilant-doodle"},
 
-  {name:"Logan Beckman", 
-   gameName:"Delta Lab",
-   url:"https://loganbeckman21.github.io/urban-invention-delta/"}
-];
+  {name:"John Blaxton", 
+   gameName:"decatur-delta",
+   url:"https://github.com/JohnAllenB/decatur-delta-lab04-blaxton"},
 
-const echoGames =[
-  {name:"Slade Berryman", 
-   gameName:"Echo Lab",
-   url:"https://sladeberryman.github.io/echo-376/"},
-  
-  {name:"Rachel Honeycutt", 
-   gameName:"Echo Lab",
-   url:"https://rachelhoneycutt.github.io/ideal-couscous-ECHO/"},
-  
-  {name:"Seungyeon Chang", 
-   gameName:"Echo Lab",
-   url:"https://seungyeonchang.github.io/echo_lab_2023/"},
+  {name:"Kalani Sorensen", 
+   gameName:"reimagined-couscous–delta",
+   url:"https://github.com/babayaga1013/reimagined-couscous--delta"},
 
-  {name:"Brandon Jacks", 
-   gameName:"Echo Lab",
-   url:"https://bjacks01.github.io/brandoEcho/"},
-  
-  {name:"Kaitlin Renfroe", 
-   gameName:"Echo Lab",
-   url:"https://kaitlinrenfroe.github.io/renfroe-echo-lab/"},
+  {name:"Jacob Vance", 
+   gameName:"fluffy-pancake-delta",
+   url:"https://jacob-vance.github.io/fluffy-pancake-delta/"},
 
-  {name:"Ryan McDonald", 
-   gameName:"Echo Lab",
-   url:"https://azyn7.github.io/Echo-Lab/"}
+  {name:"Jayden Green", 
+   gameName:"delta-jubilant-octo-winner",
+   url:"https://jaydengreen23.github.io/delta-jubilant-octo-winner/"},
+
+  {name:"Sam Maldonado", 
+   gameName:"Name Game",
+   url:"https://smaldonado1326.github.io/glowing-adventure-delta/"},
+
+  {name:"Nolen Robinson", 
+   gameName:"Delta Names",
+   url:"https://nrobinson4.github.io/cool-animal-delta/"},
+
+  {name:"Michael Duran", 
+   gameName:"The Mystical Lucky Eccentric Silly Llama Game!",
+   url:"https://github.com/MichaelHaydenDuran/chica-bonita-delta"},
+
+  {name:"Alex Glenn", 
+   gameName:"delta-project-alex",
+   url:"https://pglenn1.github.io/delta-project-alex/"},
+
+  {name:"Kevin Hernandez", 
+   gameName:"Spirit Animal Name Game",
+   url:"https://hkevin555.github.io/DeltaLab/"},
+
+  {name:"Rodnesia Goodloe", 
+   gameName:"currentDeltaLab",
+   url:"https://github.com/nesiagoodloe/currentDeltaLab"},
+
+  {name:"Clarie Mitchell", 
+   gameName:"Delta",
+   url:"https://cm1803.github.io/delta/"},
+
+  {name:"Marvin Cruz", 
+   gameName:"Delta Lab Spirit animal",
+   url:"https://marvin-2056.github.io/Delta-CIS-367/"},
+
+  {name:"Saleh Basalim", 
+   gameName:"Delta-lab",
+   url:"https://salehmb89.github.io/delta-lab/"},
+
+  {name:"Mandy McGinnis", 
+   gameName:"Spirit Animal Delta",
+   url:"https://github.com/mmandelyn/pink-panda-delta"},
+
+  {name:"Skyler Saunders", 
+   gameName:"animated-sniffle-foxtrot",
+   url:"https://skysaunders.github.io/animated-sniffle-foxtrot/"},
+
+  {name:"Candice Buford", 
+   gameName:"Glowing Giggle Echo Lab",
+   url:"https://cbuf0rd.github.io/glowing-giggle-echo-lab/"},
+
+  {name:"Mohammed Alrasheidi", 
+   gameName:"Delta",
+   url:"https://mohammedalrasheidi.github.io/Delta-lab/"},
+
+  {name:"Grant Hartley", 
+   gameName:"delta",
+   url:"https://github.com/hrantgartley/didactic-waffle-delta"},
+
+  {name:"Ayman Alluhaybi", 
+   gameName:"delta",
+   url:"https://aalluhaybi1.github.io/DELTA-LAB"},
+
+  {name:"Brayden Bolton", 
+   gameName:"Delta",
+   url:"https://github.com/bbolton1/delta-lab-brayden"},
+
+  {name:"Navraj Singh", 
+   gameName:"turbo-delta",
+   url:"https://github.com/QuantumCoder-242/turbo-delta"},
+
+  {name:"Akashdeep Sigh", 
+   gameName:"psychic-delta ",
+   url:"https://github.com/AkashdeepSingh0047/psychic-delta?tab=readme-ov-file"},
+
+  {name:"Zach Rundle", 
+   gameName:"zrundle-delta-lab",
+   url:"https://github.com/zrundle/zrundle-delta-lab"},
+
+  {name:"Isabelle Rushing", 
+   gameName:"delta",
+   url:"https://isabellerushing.github.io/crispy-octo-carnival-delta/"}
 ];
